8e60c5a1a7018c8da08ef8abe0ae3a25cfd3dfc8
PoltorakPA <paulepoltorak.com>
2023-02-15 23:05:29 +0500

    выполнил первле ДЗ

---
+++ src/main/java/ru/poltorak/alishev/rest/firstrestapp/Lesson1.java
@@ -0,0 +1,11 @@
+package ru.poltorak.alishev.rest.firstrestapp;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        task1();
+
+    }
+    private static void task1(){
+        System.out.println("Решение первой задачи");
+    }
+}
